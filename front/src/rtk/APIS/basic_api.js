5e5bb8f6bde8048e4c09f31928bf3924f60e3a1e
TalantRahimberdiev <76486068+TalantRahimberdiev@users.noreply.github.com>
2025-03-31 13:58:26 +0600

    Update basic_api.js

--- front/src/rtk/APIS/basic_api.js
+++ front/src/rtk/APIS/basic_api.js
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const emptySplitApi = createApi({
   reducerPath: 'extendedApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://10.30.18.4:8000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: '' }),
   endpoints: () => ({}),
 });
